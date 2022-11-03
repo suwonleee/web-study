cd5d386f98fec08cf8ffeb71db097e1ab13a1acd
suwonleee <suwonleee@naver.com>
2022-11-03 15:41:20 +0900

    Jquery part apply

--- todo-list/practice/practice.js
+++ todo-list/practice/practice.js
@@ -1,5 +1,7 @@
-
+/******************************/
 /* 1. 기본적인 활용법 */
+/******************************/
+
 // 변수 지정할 경우
 // 변수에는 숫자, 문자, boolean(참, 거짓) 이라는 변수 지정
 
@@ -32,11 +34,17 @@ console.log(b * b2); // 20000
 console.log(b / b2); // 0.5
 console.log(b % b2); // 100 %는 나머지
 
+
+/******************************/
 /* 2. 백틱 활용법 */
+/******************************/
 console.log(`저의 나이는 ${b}입니다`)
 
 
+
+/******************************/
 /* 3. 객체와 배열 */
+/******************************/
 let arr = [1,2,3,4,'문자',true,false]
 
 // 문자가 담겨있는걸 꺼내려면
@@ -69,3 +77,93 @@ let dic2 = [
 	{"c":"carrot"}
 ]
 console.log(obj)
+
+/******************************/
+/* 4. 정보 접근법 */
+/******************************/
+let api = {
+    "page": 1,
+    "total_results": 10000,
+    "total_pages": 500,
+    "results": [
+        {
+            "popularity" : 3130.643,
+            "vote_count" : 143,
+            "video" : false,
+            "genre_ids" : [
+                18,
+                878,
+                53
+            ],
+            "title": "2067",
+            "vote_average": 5.8
+        }
+    ]
+}
+// vote_count를 꺼내고 싶다면?
+console.log(api.results[0].vote_count); //143
+
+
+
+/******************************/
+/* 5. 반복문  */
+/******************************/
+// for 문
+
+let arr2 = [1,2,3,4,5,6,7];
+for(let i=0; i < arr2.length; i++){
+    console.log(i)
+    console.log(arr2[i])
+}
+// 0번째는 1 , 1번째는 2, ... 6번째는 7
+
+
+// 조건을 만족하는 짝수만 나오게 하기
+for(let i=0; i < arr2.length; i++){
+
+    if(arr[i] % 2 == 0){
+        console.log(arr2[i])
+    }
+}
+// 2, 4, 6
+// 만약 조건을 만족하지 않을 때
+
+// 짝수 & 4의 배수만 나오게 하기
+for(let i=0; i < arr2.length; i++){
+
+    if(arr[i] % 2 == 0 && arr2[i] % 4 == 0){
+        console.log(arr2[i])
+    }
+}
+// 4
+// 만약 조건을 만족하지 않을 때
+
+
+/******************************/
+/* 6. 함수  */
+/******************************/
+function x(num1, num2){
+    return num1 + num2;
+}
+
+let y = x(100, 200)
+console.log(y) // 300
+
+
+// 기본 제공 함수
+
+//split
+let StrEx = "자바-스크립트";
+let StrEx2 = StrEx.split("-");
+console.log(StrEx2)
+//["자바","스크립트"]
+
+//join
+let StrEx3 = StrEx2.join("*");
+console.log(StrEx3)
+//자바*스크립트
+
+//indexOf
+console.log(str.indexOf("바")) // 1 (인덱스 번호)
+console.log(str.indexOf("끼")) // -1 (인덱스에 없기 때문에 미포함)
+
