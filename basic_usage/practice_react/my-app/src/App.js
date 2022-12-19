35721648dd2c5d56e0041e71b0d5aabe174ae9e9
suwonleee <suwonleee@naver.com>
2022-12-20 00:21:05 +0900

    List.js and list app.js update

--- basic_usage/practice_react/my-app/src/App.js
+++ basic_usage/practice_react/my-app/src/App.js
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import Counter from "./pages/Counter";
 import Input from "./pages/Input";
 import Input2 from "./pages/Input2_case";
+import List from "./pages/List";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <Link to="/">Home</Link> | <Link to="/about"> About</Link> |(" ")
         <Link to="/counter">Counter</Link> |(" ")
         <Link to="/input">Input</Link> |(" ")
-        <Link to="/input2">Input2</Link>
+        <Link to="/input2">Input2</Link>|(" ")
+        <Link to="/List">List</Link>
         {/* 위 Link문은 이것과 같다. <a href="/">Home</a> */}
       </nav>
       <Routes>
@@ -24,6 +26,7 @@ function App() {
         <Route path="/counter" element={<Counter />} />
         <Route path="/input" element={<Input />} />
         <Route path="/input2" element={<Input2 />} />
+        <Route path="/list" element={<List />} />
       </Routes>
     </div>
   );
