557a260ee4d2604891d5ed516733152501133388
suwonleee <suwonleee@naver.com>
2022-12-23 18:42:13 +0900

    Add practice_javascript about const/let

---
+++ basic_usage/practice_javascript/practice.js
@@ -0,0 +1,18 @@
+//****************************** */
+//const 와 let
+
+console.clear();
+
+console.log('== let ==');
+let age1 = 20;
+console.log('age1 : ' + age1);
+age1 = 30; // 변경가능
+console.log('age1 : ' + age1);
+
+console.log('== const ==');
+const age2 = 20;
+console.log('age2 : ' + age2);
+// age2 = 30; // 불가능
+console.log('age2 : ' + age2);
+
+//****************************** */
