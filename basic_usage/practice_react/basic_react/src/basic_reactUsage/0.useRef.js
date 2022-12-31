730048cb2deb1d62d9aa0cadf1d9cbddf67d0b75
suwonleee <suwonleee@naver.com>
2022-12-31 18:20:49 +0900

    React basic usage folder was made. and added useRef example1.

---
+++ basic_usage/practice_react/basic_react/src/basic_reactUsage/0.useRef.js
@@ -0,0 +1,41 @@
+import React, { useState, useRef } from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+function App() {
+  const formInputNoRef = useRef(null); //useRef 이렇게 사용해준다.
+  const [no, setNo] = useState("");
+
+  const notice = () => {
+    formInputNoRef.current.focus(); //입력 행위 후 다시 포커스를 입력 창으로 바꿔주기
+    
+    if (!no) {
+      alert("숫자를 입력해주세요.");
+      return;
+    }
+
+    alert(`당신이 입력한 숫자는 ${no} 입니다.`);
+    setNo('');
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          notice();
+        }}
+      >
+        <input
+          ref={formInputNoRef}
+          type="text"
+          placeholder="숫자"
+          value={no}
+          onChange={(e) => setNo(e.target.value)}
+        />
+        <button>실행</button>
+      </form>
+    </>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
