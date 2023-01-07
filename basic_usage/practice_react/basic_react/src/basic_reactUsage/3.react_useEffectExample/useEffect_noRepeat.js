7e193d19043dafb747dc99d5114085c6bc006616
suwonleee <suwonleee@naver.com>
2023-01-07 23:54:37 +0900

    Add explain about the code.

--- basic_usage/practice_react/basic_react/src/basic_reactUsage/3.react_useEffectExample/useEffect_noRepeat.js
+++ basic_usage/practice_react/basic_react/src/basic_reactUsage/3.react_useEffectExample/useEffect_noRepeat.js
@@ -1,18 +1,22 @@
+// useEffect를 사용하여 함수의 특정부분 재실행 방지
+// useEffect는 리랜더링시마다 딱 1번만 실행 되어야하는 경우
+
+// ** Q. 증가 버튼을 누르면 -> 'app이 1번 실행됨!' && 증가 + 1 되게 만들기  
 
 import React, { useState, useEffect } from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
 
-let AppCallCount = 0;
+let AppCallCount = 0; //함수 실행 횟수 메소드 
 
 function App() {
-  useEffect(() => {
-    AppCallCount++;
+  useEffect(() => { // 안에 변수를 함수로 지정
+    AppCallCount++; //리랜더링 될 때 마다 App call 변수를 하나 더해주기
     console.log(`App이 ${AppCallCount}번 실행됨!`);
   }, []);
   
   const [no, setNo] = useState(0);
 
-  return <button onClick={() => setNo(no + 1)}>증가 : {no}</button>;
+  return <button onClick={() => setNo(no + 1)}>증가 : {no}</button>; //useState로 실제로 동작(리랜더링) 시키기 
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
