ee80d348d84c84782a29c4c07a972c7749aefb9a
suwonleee <suwonleee@naver.com>
2023-01-08 23:44:54 +0900

    useEffect slowTempo explain comments added.

--- basic_usage/practice_react/basic_react/src/basic_reactUsage/3.react_useEffectExample/1.useEffect_slowTempo.js
+++ basic_usage/practice_react/basic_react/src/basic_reactUsage/3.react_useEffectExample/1.useEffect_slowTempo.js
@@ -1,6 +1,7 @@
 // 의존성 배열 인자를 제거하면, 매번 실행은 되지만 타이밍이 늦어진다.
+// 원래 순서는 [ 시작 -> App이 n번 실행됨 -> 끝 ] 순서일텐데, useEffect 덕분에 한 템포 느리게 실행된다.
 
-// ** Q. 증가 버튼을 누르면 -> 시작 -> 끝 -> app이 1번 실행됨! && 증가 =+ 1 되게 만들기  
+// ** Q. 증가 버튼을 누르면 -> [ 시작 -> 끝 -> app이 1번 실행됨! ] && 증가 =+ 1 되게 만들기  
 
 import React, { useState, useEffect } from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
@@ -13,9 +14,12 @@ function App() {
   useEffect(() => {
     AppCallCount++;
     console.log(`App이 ${AppCallCount}번 실행됨!`);
-  });
+  }); 
+  // }, []); -> 이렇게 코드 적으면, 딱 한번만 실행된다 [] 이 리스트 때문에.
   
   console.log('끝');
+
+  // 시작 -> 끝 -> app이 1번 실행됨
   
   const [no, setNo] = useState(0);
 
